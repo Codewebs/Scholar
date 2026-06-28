a390f8afbab8ad858a77fcd43500cfc359af8c19
Envy <landryhion@gmail.com>
2026-06-28 18:27:00 +0100

    mise en production 07 sur render

--- Node/routes/menus.js
+++ Node/routes/menus.js
@@ -6,7 +6,7 @@ const db = require("../db");
 const Menu = require('../models/menu');
 const AutorisationUtilisateur = require('../models/autorisation_utilisateur');
 const Qualite = require('../models/qualite');
-const Utilisateur = require('../models/utilisateur');
+const Utilisateur = require('../models/Utilisateur');
 const { verifyToken } = require("../middleware/auth");
 require("dotenv").config();
 
